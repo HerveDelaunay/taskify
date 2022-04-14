3b99954fa4a386734ed2a362d367295da6f2b780
HerveDelaunay <delaunay.herve06@gmail.com>
2022-04-14 20:36:57 +0200

    css & responsive wip

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './styles.css';
 
 interface Props{
@@ -7,10 +7,17 @@ interface Props{
     handleAdd: (event: React.FormEvent) => void
 }
 
-const InputField = ({todo, setTodo, handleAdd}: Props) => {
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input" onSubmit={handleAdd}>
-        <input type="input" 
+    <form className="input" onSubmit={(event)=> {
+        handleAdd(event);
+        inputRef.current?.blur();
+    }}>
+        <input 
+        ref={inputRef}
+        type="input" 
         value={todo}
         onChange = {(event) => setTodo(event.target.value)}
         placeholder='Enter a task' className='input__box'
