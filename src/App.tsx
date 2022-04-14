8e0700c938e99a25d0ef14f6a165f04488845c60
HerveDelaunay <delaunay.herve06@gmail.com>
2022-04-14 17:32:39 +0200

    input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">Hello world</div>
-  );
-}
+  <div className="App">
+    <span className='heading'>Taskify</span>
+    <InputField/>
+  </div>
+);
+};
 
 export default App;
---
+++ src/components/InputField.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputField = () => {
+  return (
+    <form className="input">
+        <input type="input" placeholder='enter a task'/>
+    </form>
+  )
+}
+
+export default InputField
