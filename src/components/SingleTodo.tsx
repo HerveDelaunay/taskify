066a5cd2cb36ddaaf7919761d5453a1dd44982a2
HerveDelaunay <delaunay.herve06@gmail.com>
2022-04-24 15:56:16 +0200

    feat-editTask: done

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Todo } from '../model'
 import { AiFillEdit, AiFillDelete } from "react-icons/ai"
 import { MdDone } from "react-icons/md"
@@ -13,6 +13,7 @@ type Props = {
 const SingleTodo = ({todo, todos, setTodos}: Props) => {
   const [edit, setEdit] = useState<boolean>(false)
   const [editTodo, setEditTodo] = useState<string>(todo.todo)
+  const inputRef = useRef<HTMLInputElement>(null);
   const handleDone = (id: number) => {
     setTodos(todos.map((todo) => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo))
   }
@@ -24,13 +25,17 @@ const SingleTodo = ({todo, todos, setTodos}: Props) => {
     setTodos(todos.map((todo) => todo.id === id ? { ...todo, todo: editTodo } : todo))
     setEdit(false)
   }
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [edit])
 
   return (
     <form className='todos__single' onSubmit={(event) =>{
       handleEdit(event, todo.id)
     }}>
       { edit ? 
-        <input type="text" value={editTodo} 
+        <input type="text" value={editTodo}
+        ref={inputRef} 
         onChange={(event) => setEditTodo(event.target.value)}
         className="todos__single--text"
         /> :
